8f34176ed1d909b67345c1cac4b28e0784ea68d9
chirvasitua <chirvasitua@gmail.com>
2021-06-23 18:51:28 -0400

    use 'env perl' in place of system perl

--- script/trser.pl
+++ script/trser.pl
@@ -1,13 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use warnings;
 use v5.12;
 
 use Data::Dumper;
-use lib qq!$ENV{HOME}/repos/tree-serial/lib!;
 use Tree::Serial;
-use List::Util qw(zip);
 
 say Dumper(Tree::Serial->new());
-## say Dumper(Tree::Serial->new({traversal => 0})->strs2lol(\@ARGV));
+say Dumper(Tree::Serial->new({degree => 3, traversal => 2})->strs2lol(\@ARGV));
 
 ## say Dumper(Tree::Serial->new({degree => 3})->strs2hash(\@ARGV));
